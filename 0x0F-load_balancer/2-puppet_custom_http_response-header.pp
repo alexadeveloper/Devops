4a7f318a6cc7fcb0076be8da1d704e9dcf0b6a99
Alexa <alexafdeveloper@gmail.com>
2019-08-18 01:37:23 +0000

    test task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,13 +2,21 @@
 $ruta = '/etc/nginx/nginx.conf'
 $linea = "http {\n\tadd_header X-Served-By \"${hostname}\";"
 $comando = '/usr/sbin/service nginx start'
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
 package {'nginx':
          ensure => 'present',
+         require => Exec['apt-get update'],
 }
 -> file_line { 'http_header':
   path  => $ruta,
   match => 'http {',
   line  => $linea,
+}
+->service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
 -> exec {'run2':
   command => $comando,
