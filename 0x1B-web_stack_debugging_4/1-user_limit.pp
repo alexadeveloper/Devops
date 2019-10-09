6621fa26a59b417499f35543dd3f360f08d41068
Alexa <alexafdeveloper@gmail.com>
2019-10-09 17:10:48 +0000

    hard & soft files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,15 @@
+#make changes the limit
+exec { 'replace hard':
+ cwd => '/etc/security/',
+ command => "sed -i -e 's/holberton hard nofile 5/holberton hard nofile 2048/g' limits.conf",
+ path => ['/bin','/usr/bin','/usr/sbin'],
+}->
+exec { 'replace soft':
+ cwd => '/etc/security/',
+ command => "sed -i -e 's/holberton soft nofile 4/holberton soft nofile 2048/g' limits.conf",
+ path => ['/bin','/usr/bin','/usr/sbin'],
+}->
+exec { 'restart':
+ command => 'sudo sysctl -p',
+ path => ['/usr/bin', '/usr/sbin','/bin'],
+}
