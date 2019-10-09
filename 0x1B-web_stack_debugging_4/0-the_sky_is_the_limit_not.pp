acd0568ab75db45b7314b9eef8e4188a35a3df0b
Alexa <alexafdeveloper@gmail.com>
2019-10-09 16:51:41 +0000

    change the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+#make changes the limit
+exec { 'replace':
+ cwd => '/etc/default/',
+ command => "sed -i -e 's/15/2048/g' nginx",
+ path => ['/bin','/usr/bin','/usr/sbin'],
+}
+exec { 'restart':
+ command => 'sudo service nginx restart',
+ path => ['/usr/bin', '/usr/sbin','/bin'],
+}
